13a9a21d7766dbea4b0e185bd43d7de01a2445f5
nokken65 <unknown65182@gmail.com>
2024-03-01 23:21:38 +0300

    fix: try to fix router

--- src/app/model/router.tsx
+++ src/app/model/router.tsx
@@ -2,6 +2,6 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { routesObj } from '@/pages/routing'
 
-const router = createBrowserRouter(routesObj)
+const router = createBrowserRouter(routesObj, { basename: '/counter' })
 
 export { router }
